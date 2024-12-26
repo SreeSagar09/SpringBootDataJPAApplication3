069c016d572f2917966259fae919e09872d8fc2d
SreeSagar09 <sreesagarmamidala09@gmail.com>
2024-12-26 21:22:23 +0530

    SpringBootDataJPAApplication2ApplicationTests file name changed to SpringBootDataJPAApplication3ApplicationTests

--- src/test/java/com/app/SpringBootDataJPAApplication3ApplicationTests.java
+++ src/test/java/com/app/SpringBootDataJPAApplication3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootDataJPAApplication2ApplicationTests {
+class SpringBootDataJPAApplication3ApplicationTests {
 
 	@Test
 	void contextLoads() {
